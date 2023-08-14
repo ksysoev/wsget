b4ea28c0f23b0740c7b287263b52081b246e4c97
Кирилл Сысоев <k.sysoev@me.com>
2023-08-14 16:39:43 +0800

    Adds basic implement with reading request from stdin and print single response to stdout

--- cmd/wsget/main.go
+++ cmd/wsget/main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
 
+	"golang.org/x/net/websocket"
+)
+
+var wsUrl string
+
+func init() {
+	url := flag.String("u", "", "ws url")
+	flag.Parse()
+
+	if url == nil || *url == "" {
+		log.Fatal("url is requered")
+	}
+
+	wsUrl = *url
+}
+
+func main() {
+	ws, err := websocket.Dial(wsUrl, "", "http://localhost")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer ws.Close()
+
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+
+	err = websocket.Message.Send(ws, input)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var msg string
+	err = websocket.Message.Receive(ws, &msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(msg)
 }
