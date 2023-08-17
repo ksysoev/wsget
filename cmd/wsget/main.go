98b0fc9fd3dd6b5ef676e82d9c6829568dfd7372
Кирилл Сысоев <k.sysoev@me.com>
2023-08-17 22:54:00 +0800

    Raw implementation for handling keyboard input

--- cmd/wsget/main.go
+++ cmd/wsget/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/TylerBrock/colorjson"
+	"github.com/eiannone/keyboard"
 	"golang.org/x/net/websocket"
 )
 
@@ -70,11 +69,17 @@ func (wsInsp *WSInspector) Close() {
 	wsInsp.ws.Close()
 }
 
+type View struct {
+	state string
+}
+
 func main() {
+	fmt.Println("Connecting to", wsUrl, "...")
 	wsInsp, err := NewWSInspector(wsUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("Connected")
 	defer wsInsp.Close()
 
 	go func() {
@@ -89,14 +94,93 @@ func main() {
 		return
 	}()
 
+	fmt.Println("to enter to request mode press ESC")
 	for {
-		reader := bufio.NewReader(os.Stdin)
-		input, _ := reader.ReadString('\n')
-
-		err = wsInsp.Send(input)
-
-		if err != nil {
-			log.Fatal(err)
+		if err := keyboard.Open(); err != nil {
+			panic(err)
 		}
+
+		_, key, err := keyboard.GetSingleKey()
+		if err != nil {
+			panic(err)
+		}
+
+		if key == keyboard.KeyCtrlC || key == keyboard.KeyCtrlD {
+			return
+		}
+
+		if key == keyboard.KeyEsc {
+			keyboard.Close()
+			fmt.Println("Request mode. Press Ctrl+S to send request. Press ESC to cancel request")
+
+			keysEvents, err := keyboard.GetKeys(10)
+			if err != nil {
+				panic(err)
+			}
+			buffer := ""
+			for {
+				event := <-keysEvents
+				if event.Err != nil {
+					panic(event.Err)
+				}
+
+				if key == keyboard.KeyCtrlC || key == keyboard.KeyCtrlD {
+					return
+				}
+
+				if event.Key == keyboard.KeyCtrlS {
+					break
+				}
+
+				if event.Key == keyboard.KeyEsc {
+					buffer = ""
+					break
+				}
+
+				if event.Key == keyboard.KeySpace {
+					fmt.Print(" ")
+					buffer += " "
+					continue
+				}
+
+				if event.Key == keyboard.KeyEnter {
+					fmt.Print("\n")
+					buffer += "\n"
+					continue
+				}
+
+				if event.Key == keyboard.KeyBackspace || keyboard.KeyDelete == event.Key || event.Key == 127 {
+					if len(buffer) == 0 {
+						continue
+					}
+
+					if buffer[len(buffer)-1] == '\n' {
+						continue
+					}
+
+					fmt.Print("\b \b")
+					buffer = buffer[:len(buffer)-1]
+					continue
+				}
+
+				if event.Key > 0 {
+					continue
+				}
+
+				fmt.Print(string(event.Rune))
+
+				buffer += string(event.Rune)
+			}
+
+			if buffer == "" {
+				continue
+			}
+
+			err = wsInsp.Send(buffer)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		keyboard.Close()
 	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/TylerBrock/colorjson v0.0.0-20200706003622-8a50f05110d2 // indirect
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
 	github.com/fatih/color v1.15.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
