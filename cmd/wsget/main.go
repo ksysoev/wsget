49275ba274d4af409e3d0a88d9bdfd52eced083f
Кирилл Сысоев <k.sysoev@me.com>
2023-08-19 22:36:04 +0800

    Reorganizing mode handling

--- cmd/wsget/main.go
+++ cmd/wsget/main.go
@@ -69,10 +69,6 @@ func (wsInsp *WSInspector) Close() {
 	wsInsp.ws.Close()
 }
 
-type View struct {
-	state string
-}
-
 func main() {
 	fmt.Println("Connecting to", wsUrl, "...")
 	wsInsp, err := NewWSInspector(wsUrl)
@@ -82,105 +78,106 @@ func main() {
 	fmt.Println("Connected")
 	defer wsInsp.Close()
 
-	go func() {
-		f := colorjson.NewFormatter()
-		f.Indent = 2
-		for msg := range wsInsp.messages {
+	f := colorjson.NewFormatter()
+	f.Indent = 2
+
+	if err := keyboard.Open(); err != nil {
+		panic(err)
+	}
+	defer keyboard.Close()
+
+	keysEvents, err := keyboard.GetKeys(10)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connection Mode: Press ESC to enter Request mode")
+	for {
+		select {
+		case event := <-keysEvents:
+			switch event.Key {
+			case keyboard.KeyCtrlC, keyboard.KeyCtrlD:
+				return
+
+			case keyboard.KeyEsc:
+				fmt.Println("Request Mode: Type your API request and press Ctrl+S to send it. Press ESC to cancel request")
+				req, err := requestMode(keysEvents)
+
+				if err != nil {
+					if err.Error() == "interrupted" {
+						return
+					}
+
+					fmt.Println(err)
+				}
+
+				if req != "" {
+					err = wsInsp.Send(req)
+					if err != nil {
+						fmt.Println("Fail to send request:", err)
+					}
+				}
+
+				fmt.Println("Connection Mode: Press ESC to enter Request mode")
+			}
+
+		case msg := <-wsInsp.messages:
 			var obj any
 			json.Unmarshal([]byte(msg), &obj)
 			s, _ := f.Marshal(obj)
-			fmt.Println(string(s), "\n")
+			fmt.Printf("%s\n\n", string(s))
 		}
-		return
-	}()
+	}
+}
 
-	fmt.Println("to enter to request mode press ESC")
-	for {
-		if err := keyboard.Open(); err != nil {
-			panic(err)
-		}
-
-		_, key, err := keyboard.GetSingleKey()
-		if err != nil {
-			panic(err)
+func requestMode(keyStream <-chan keyboard.KeyEvent) (string, error) {
+	buffer := ""
+	for e := range keyStream {
+		if e.Err != nil {
+			return buffer, e.Err
 		}
 
-		if key == keyboard.KeyCtrlC || key == keyboard.KeyCtrlD {
-			return
-		}
-
-		if key == keyboard.KeyEsc {
-			keyboard.Close()
-			fmt.Println("Request mode. Press Ctrl+S to send request. Press ESC to cancel request")
-
-			keysEvents, err := keyboard.GetKeys(10)
-			if err != nil {
-				panic(err)
-			}
-			buffer := ""
-			for {
-				event := <-keysEvents
-				if event.Err != nil {
-					panic(event.Err)
-				}
-
-				if key == keyboard.KeyCtrlC || key == keyboard.KeyCtrlD {
-					return
-				}
-
-				if event.Key == keyboard.KeyCtrlS {
-					break
-				}
-
-				if event.Key == keyboard.KeyEsc {
-					buffer = ""
-					break
-				}
-
-				if event.Key == keyboard.KeySpace {
-					fmt.Print(" ")
-					buffer += " "
-					continue
-				}
-
-				if event.Key == keyboard.KeyEnter {
-					fmt.Print("\n")
-					buffer += "\n"
-					continue
-				}
-
-				if event.Key == keyboard.KeyBackspace || keyboard.KeyDelete == event.Key || event.Key == 127 {
-					if len(buffer) == 0 {
-						continue
-					}
-
-					if buffer[len(buffer)-1] == '\n' {
-						continue
-					}
-
-					fmt.Print("\b \b")
-					buffer = buffer[:len(buffer)-1]
-					continue
-				}
-
-				if event.Key > 0 {
-					continue
-				}
-
-				fmt.Print(string(event.Rune))
-
-				buffer += string(event.Rune)
-			}
-
+		switch {
+		case e.Key == keyboard.KeyCtrlC, e.Key == keyboard.KeyCtrlD:
+			return buffer, fmt.Errorf("interrupted")
+		case e.Key == keyboard.KeyCtrlS:
 			if buffer == "" {
+				return buffer, fmt.Errorf("cannot send empty request")
+			}
+			return buffer, nil
+		case e.Key == keyboard.KeyEsc:
+			return "", nil
+
+		case e.Key == keyboard.KeySpace:
+			fmt.Print(" ")
+			buffer += " "
+			continue
+
+		case e.Key == keyboard.KeyEnter:
+			fmt.Print("\n")
+			buffer += "\n"
+			continue
+
+		case e.Key == keyboard.KeyBackspace, e.Key == keyboard.KeyDelete, e.Key == 127:
+			if len(buffer) == 0 {
 				continue
 			}
 
-			err = wsInsp.Send(buffer)
-			if err != nil {
-				log.Fatal(err)
+			if buffer[len(buffer)-1] == '\n' {
+				continue
 			}
+
+			fmt.Print("\b \b")
+			buffer = buffer[:len(buffer)-1]
+			continue
+		case e.Key > 0:
+			// Ignore rest of special keys
+			continue
+		default:
+			fmt.Print(string(e.Rune))
+			buffer += string(e.Rune)
 		}
-		keyboard.Close()
 	}
+
+	return buffer, fmt.Errorf("keyboard stream was unexpectably closed")
 }
