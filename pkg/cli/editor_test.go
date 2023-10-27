fad005bc3c8758297b37894a8c347a70df6f1ee2
Кирилл Сысоев <k.sysoev@me.com>
2023-10-27 22:20:49 +0800

    Adds tests for editor

--- pkg/cli/editor_test.go
+++ pkg/cli/editor_test.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"bytes"
 	"os"
 	"testing"
 
@@ -8,7 +9,7 @@ import (
 )
 
 func TestNewEditor(t *testing.T) {
-	output := os.Stdout
+	output := new(bytes.Buffer)
 	history := NewHistory("", 0)
 	editor := NewEditor(output, history)
 
@@ -41,7 +42,7 @@ func TestEditRequest(t *testing.T) {
 
 	defer os.Remove(tmpfile.Name())
 
-	output := os.Stdout
+	output := new(bytes.Buffer)
 	history := NewHistory(tmpfile.Name(), 5)
 	editor := NewEditor(output, history)
 
@@ -65,4 +66,155 @@ func TestEditRequest(t *testing.T) {
 	if req != "request" {
 		t.Errorf("Expected empty request, got %s", req)
 	}
+}
+
+func TestEditRequestInterrupted(t *testing.T) {
+	tmpfile, err := os.CreateTemp("", "test_history")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove(tmpfile.Name())
+
+	output := new(bytes.Buffer)
+	history := NewHistory(tmpfile.Name(), 5)
+	editor := NewEditor(output, history)
+
+	keyStream := make(chan keyboard.KeyEvent)
+	defer close(keyStream)
+
+	go func() {
+		keyStream <- keyboard.KeyEvent{Key: keyboard.KeyCtrlC}
+	}()
+
+	req, err := editor.EditRequest(keyStream, "")
+
+	if err == nil {
+		t.Error("Expected error")
+	}
+
+	if req != "" {
+		t.Errorf("Expected empty request, got %s", req)
+	}
+
+	go func() {
+		keyStream <- keyboard.KeyEvent{Key: keyboard.KeyCtrlD}
+	}()
+
+	req, err = editor.EditRequest(keyStream, "")
+
+	if err == nil {
+		t.Error("Expected error")
+	}
+
+	if req != "" {
+		t.Errorf("Expected empty request, got %s", req)
+	}
+}
+
+func TestEditRequestExitEditor(t *testing.T) {
+	tmpfile, err := os.CreateTemp("", "test_history")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove(tmpfile.Name())
+
+	output := new(bytes.Buffer)
+	history := NewHistory(tmpfile.Name(), 5)
+	editor := NewEditor(output, history)
+
+	keyStream := make(chan keyboard.KeyEvent)
+	defer close(keyStream)
+
+	go func() {
+		keyStream <- keyboard.KeyEvent{Key: keyboard.KeyEsc}
+	}()
+
+	req, err := editor.EditRequest(keyStream, "")
+
+	if err != nil {
+		t.Error("Expected no error")
+	}
+
+	if req != "" {
+		t.Errorf("Expected empty request, got %s", req)
+	}
+}
+
+func TestEditRequestClosingKeyboard(t *testing.T) {
+	tmpfile, err := os.CreateTemp("", "test_history")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove(tmpfile.Name())
+
+	output := new(bytes.Buffer)
+	history := NewHistory(tmpfile.Name(), 5)
+	editor := NewEditor(output, history)
+
+	keyStream := make(chan keyboard.KeyEvent)
+	close(keyStream)
+
+	req, err := editor.EditRequest(keyStream, "")
+
+	if err == nil {
+		t.Error("Expected error")
+	}
+
+	if req != "" {
+		t.Errorf("Expected empty request, got %s", req)
+	}
+}
+
+func TestEditRequestSpecialKeys(t *testing.T) {
+	tmpfile, err := os.CreateTemp("", "test_history")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove(tmpfile.Name())
+
+	output := new(bytes.Buffer)
+	history := NewHistory(tmpfile.Name(), 5)
+	editor := NewEditor(output, history)
+
+	keyStream := make(chan keyboard.KeyEvent)
+
+	go func() {
+		for _, key := range []keyboard.Key{
+			keyboard.KeySpace,
+			keyboard.KeyCtrlU,
+			keyboard.KeyEsc,
+		} {
+			keyStream <- keyboard.KeyEvent{Key: key}
+		}
+	}()
+
+	req, err := editor.EditRequest(keyStream, "")
+
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if req != "" {
+		t.Errorf("Expected empty request, got %s", req)
+	}
+
+	outputStr := output.String()
+
+	if outputStr != " "+LineClear+"\r" {
+		t.Errorf("Unexpected output: %q", outputStr)
+	}
+
+	go func() {
+		for _, key := range []keyboard.Key{
+			keyboard.KeySpace,
+			keyboard.KeyCtrlU,
+			keyboard.KeyEsc,
+		} {
+			keyStream <- keyboard.KeyEvent{Key: key}
+		}
+	}()
 }
