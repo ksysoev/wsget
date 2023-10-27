d254414ae3501611fd13e88dcb2b67c2395a87f0
Кирилл Сысоев <k.sysoev@me.com>
2023-10-27 21:32:31 +0800

    Improve test coveredge for ws handling

--- pkg/ws/ws_test.go
+++ pkg/ws/ws_test.go
@@ -41,3 +41,47 @@ func TestNewWS(t *testing.T) {
 		t.Errorf("Expected message, but got none")
 	}
 }
+
+func TestNewWSWithInvalidURL(t *testing.T) {
+	_, err := NewWS("invalid", Options{})
+
+	if err == nil {
+		t.Fatalf("Expected error, but got nil")
+	}
+}
+
+func TestNewWSFailToConnect(t *testing.T) {
+	_, err := NewWS("ws://localhost:12345", Options{})
+
+	if err == nil {
+		t.Fatalf("Expected error, but got nil")
+	}
+}
+
+func TestNewWSDisconnect(t *testing.T) {
+	server := httptest.NewServer(websocket.Handler(func(ws *websocket.Conn) {
+		var msg string
+		_ = websocket.Message.Receive(ws, &msg) // wait for request
+		_, _ = ws.Write([]byte(msg))
+		time.Sleep(time.Second) // to keep the connection open
+	}))
+	defer server.Close()
+
+	url := "ws://" + server.Listener.Addr().String()
+	ws, err := NewWS(url, Options{})
+
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+
+	ws.Close()
+
+	select {
+	case _, ok := <-ws.Messages:
+		if ok {
+			t.Errorf("Expected channel to be closed")
+		}
+	case <-time.After(time.Millisecond * 10):
+		t.Errorf("Expected channel to be closed")
+	}
+}
