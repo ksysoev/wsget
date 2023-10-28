c6ffb0deb8bc32225435cf6e8fb0e6ddcabd80bb
Кирилл Сысоев <k.sysoev@me.com>
2023-10-28 20:16:30 +0800

    Improves help documentation

--- cmd/wsget/main.go
+++ cmd/wsget/main.go
@@ -14,17 +14,42 @@ var insecure bool
 var request string
 var outputFile string
 
+const (
+	LongDescription = `A command-line tool for interacting with WebSocket servers.
+
+The tool have severl modes of operation:
+
+1. Request mode. The tool will start in interactive mode if no request is provided:
+
+- You can type resquest and press Ctrl+S to send it to the server. 
+- It supports multiline input.
+- You can use Ctrl+U to clear the input.
+- You can use Ctrl+C or Ctrl+D to exit the tool.
+- You can use Esc to exit Request mode and switch to connection mode.
+
+2. Connection mode. The tool will start in connection mode if request is provided.
+In this request mode the tool will send the request to the server and print responses. 
+
+- You can use Ctrl+C or Ctrl+D to exit the tool.
+- You can use Esc to switch to Request mode.
+`
+)
+
 func main() {
 	cmd := &cobra.Command{
-		Use:   "wsget [url]",
-		Short: "A command-line tool for sending WebSocket requests",
-		Args:  cobra.ExactArgs(1),
-		Run:   run,
+		Use:        "wsget url [flags]",
+		Short:      "A command-line tool for interacting with WebSocket servers",
+		Long:       LongDescription,
+		Example:    `wsget wss://ws.postman-echo.com/raw -r "Hello, world!"`,
+		Args:       cobra.ExactArgs(1),
+		ArgAliases: []string{"url"},
+		Version:    "0.1.4",
+		Run:        run,
 	}
 
 	cmd.Flags().BoolVarP(&insecure, "insecure", "k", false, "Skip SSL certificate verification")
-	cmd.Flags().StringVarP(&request, "request", "r", "", "WebSocket request")
-	cmd.Flags().StringVarP(&outputFile, "output", "o", "", "Output file")
+	cmd.Flags().StringVarP(&request, "request", "r", "", "WebSocket request that will be sent to the server")
+	cmd.Flags().StringVarP(&outputFile, "output", "o", "", "Output file for saving all request and responses")
 
 	err := cmd.Execute()
 	if err != nil {
