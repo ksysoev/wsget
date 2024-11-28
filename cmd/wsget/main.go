c8cf406ac283d45ce22294fd538e49149d6a1077
Kirill Sysoev <k.sysoev@me.com>
2024-11-28 22:20:50 +0800

    Refactor command initialization and improve connection handling in wsget

--- cmd/wsget/main.go
+++ cmd/wsget/main.go
@@ -2,150 +2,23 @@ package main
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
-	"github.com/fatih/color"
-	"github.com/ksysoev/wsget/pkg/cli"
-	"github.com/ksysoev/wsget/pkg/clierrors"
-	"github.com/ksysoev/wsget/pkg/command"
-	"github.com/ksysoev/wsget/pkg/ws"
-	"github.com/spf13/cobra"
+	"github.com/ksysoev/wsget/pkg/cmd"
 )
 
-var insecure bool
-var request string
-var outputFile string
-var inputFile string
-var headers []string
-var waitResponse int
-var verbose bool
-var Version = "dev"
-
-const (
-	LongDescription = `A command-line tool for interacting with WebSocket servers.
-
-The tool have severl modes of operation:
-
-1. Request mode. The tool will start in interactive mode if no request is provided:
-
-- You can type resquest and press Enter to send it to the server. 
-- Request editor allows to input multiline request. the last sybmol of line should be \(backslash) to indicate that the request is not finished yet.
-- You can use Ctrl+U to clear the input.
-- You can use Ctrl+C or Ctrl+D to exit the tool.
-- You can use Esc to cancel input and return to connection mod.
-
-2. Connection mode. The tool will start in connection mode if request is provided.
-In this request mode the tool will send the request to the server and print responses. 
-
-- You can use Enter to switch to request input mode.
-- You can use Esc to exit connection
-- You can use Ctrl+C or Ctrl+D to exit the tool.
-`
-)
+var version = "dev"
 
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 
-	cmd := &cobra.Command{
-		Use:        "wsget url [flags]",
-		Short:      "A command-line tool for interacting with WebSocket servers",
-		Long:       LongDescription,
-		Example:    `wsget wss://ws.postman-echo.com/raw -r "Hello, world!"`,
-		Args:       cobra.ExactArgs(1),
-		ArgAliases: []string{"url"},
-		Version:    Version,
-		Run:        run,
-	}
-
-	cmd.Flags().BoolVarP(&insecure, "insecure", "k", false, "Skip SSL certificate verification")
-	cmd.Flags().StringVarP(&request, "request", "r", "", "WebSocket request that will be sent to the server")
-	cmd.Flags().StringVarP(&outputFile, "output", "o", "", "Output file for saving all request and responses")
-	cmd.Flags().IntVarP(&waitResponse, "wait-resp", "w", -1, "Timeout for single response in seconds, 0 means no timeout. If this option is set, the tool will exit after receiving the first response")
-	cmd.Flags().StringSliceVarP(&headers, "header", "H", []string{}, "HTTP headers to attach to the request")
-	cmd.Flags().StringVarP(&inputFile, "input", "i", "", "Input YAML file with list of requests to send to the server")
-	cmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Verbose output")
-
-	if err := cmd.ExecuteContext(ctx); err != nil {
-		fmt.Println(err)
+	c := cmd.InitCommands(version)
+	if err := c.ExecuteContext(ctx); err != nil {
 		cancel()
 		os.Exit(1)
 	}
 
 	cancel()
-}
-
-func run(cmnd *cobra.Command, args []string) {
-	wsURL := args[0]
-	if wsURL == "" {
-		_ = cmnd.Help()
-		return
-	}
-
-	if waitResponse >= 0 && request == "" {
-		color.New(color.FgRed).Println("Single response timeout could be used only with request")
-		return
-	}
-
-	wsConn, err := ws.NewWS(
-		cmnd.Context(),
-		wsURL,
-		ws.Options{
-			SkipSSLVerification: insecure,
-			Headers:             headers,
-			Verbose:             verbose,
-		},
-	)
-	if err != nil {
-		color.New(color.FgRed).Println("Unable to connect to the server: ", err)
-		return
-	}
-
-	defer wsConn.Close()
-
-	input := cli.NewKeyboard()
-
-	client, err := cli.NewCLI(wsConn, input, os.Stdout)
-	if err != nil {
-		color.New(color.FgRed).Println("Unable to start CLI: ", err)
-		return
-	}
-
-	opts := cli.RunOptions{}
-
-	if outputFile != "" {
-		if opts.OutputFile, err = os.Create(outputFile); err != nil {
-			color.New(color.FgRed).Println("Fail to open output file: ", err)
-			return
-		}
-	}
-
-	switch {
-	case request != "":
-		opts.Commands = []command.Executer{command.NewSend(request)}
-
-		if waitResponse >= 0 {
-			opts.Commands = append(
-				opts.Commands,
-				command.NewWaitForResp(time.Duration(waitResponse)*time.Second),
-				command.NewExit(),
-			)
-		}
-	case inputFile != "":
-		opts.Commands = []command.Executer{command.NewInputFileCommand(inputFile)}
-	default:
-		opts.Commands = []command.Executer{command.NewEdit("")}
-	}
-
-	if err = client.Run(opts); err != nil {
-		if errors.As(err, &clierrors.Interrupted{}) {
-			return
-		}
-
-		color.New(color.FgRed).Println(err)
-	}
 }
--- pkg/cmd/connect.go
+++ pkg/cmd/connect.go
@@ -1,9 +1,84 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"time"
 
-func runConnectCmd(arg *flags) func(cmd *cobra.Command, args []string) error {
-	return func(cmd *cobra.Command, args []string) error {
+	"github.com/ksysoev/wsget/pkg/cli"
+	"github.com/ksysoev/wsget/pkg/clierrors"
+	"github.com/ksysoev/wsget/pkg/command"
+	"github.com/ksysoev/wsget/pkg/ws"
+	"github.com/spf13/cobra"
+)
+
+func runConnectCmd(args *flags) func(cmd *cobra.Command, args []string) error {
+	return func(cmd *cobra.Command, unnamedArgs []string) error {
+		wsURL := unnamedArgs[0]
+		if wsURL == "" {
+			return fmt.Errorf("url is required")
+		}
+
+		if args.waitResponse >= 0 && args.request == "" {
+			return fmt.Errorf("single response timeout could be used only with request")
+		}
+
+		wsConn, err := ws.NewWS(
+			cmd.Context(),
+			wsURL,
+			ws.Options{
+				SkipSSLVerification: args.insecure,
+				Headers:             args.headers,
+				Verbose:             args.verbose,
+			},
+		)
+		if err != nil {
+			return fmt.Errorf("unable to connect to the server: %w", err)
+		}
+
+		defer wsConn.Close()
+
+		input := cli.NewKeyboard()
+
+		client, err := cli.NewCLI(wsConn, input, os.Stdout)
+		if err != nil {
+			return fmt.Errorf("unable to start CLI: %w", err)
+		}
+
+		opts := cli.RunOptions{}
+
+		if args.outputFile != "" {
+			if opts.OutputFile, err = os.Create(args.outputFile); err != nil {
+				return fmt.Errorf("fail to open output file: %w", err)
+			}
+		}
+
+		switch {
+		case args.request != "":
+			opts.Commands = []command.Executer{command.NewSend(args.request)}
+
+			if args.waitResponse >= 0 {
+				opts.Commands = append(
+					opts.Commands,
+					command.NewWaitForResp(time.Duration(args.waitResponse)*time.Second),
+					command.NewExit(),
+				)
+			}
+		case args.inputFile != "":
+			opts.Commands = []command.Executer{command.NewInputFileCommand(args.inputFile)}
+		default:
+			opts.Commands = []command.Executer{command.NewEdit("")}
+		}
+
+		if err = client.Run(opts); err != nil {
+			if errors.As(err, &clierrors.Interrupted{}) {
+				return nil
+			}
+
+			return fmt.Errorf("failed to run client: %w", err)
+		}
+
 		return nil
 	}
 }
