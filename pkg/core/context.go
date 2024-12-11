2a2e2e9e02cf6afc9cc269b38a299274a91eb988
Kirill Sysoev <k.sysoev@me.com>
2024-12-11 22:01:58 +0800

    Refactor executionContext to enhance CLI message handling
    Added methods for printing formatted messages, sending requests, and handling responses with timeouts. Improved output handling by supporting colored messages and output files. These changes enhance flexibility and maintainability in CLI operations.

--- pkg/core/context.go
+++ pkg/core/context.go
@@ -2,12 +2,18 @@ package core
 
 import (
 	"context"
+	"fmt"
 	"io"
+	"reflect"
+	"time"
+
+	"github.com/fatih/color"
 )
 
 type executionContext struct {
 	cli        *CLI
 	outputFile io.Writer
+	ctx        context.Context
 }
 
 func newExecutionContext(cli *CLI, outputFile io.Writer) *executionContext {
@@ -17,30 +23,69 @@ func newExecutionContext(cli *CLI, outputFile io.Writer) *executionContext {
 	}
 }
 
-func (ctx *executionContext) OutputFile() io.Writer {
-	return ctx.outputFile
+func (c *executionContext) Print(data string) error {
+	_, err := fmt.Fprintln(c.cli.output, data)
+	return err
 }
 
-func (ctx *executionContext) Output() io.Writer {
-	return ctx.cli.output
+func (c *executionContext) PrintMessage(msg Message) error {
+	output, err := c.cli.formater.FormatMessage(msg.Type.String(), msg.Data)
+
+	if err != nil {
+		return err
+	}
+
+	switch msg.Type {
+	case Request:
+		_, _ = color.New(color.FgGreen).Fprintln(c.cli.output, "->")
+	case Response:
+		_, _ = color.New(color.FgRed).Fprintln(c.cli.output, "<-")
+	default:
+		return fmt.Errorf("unsupported message type: %s", msg.Type.String())
+	}
+
+	_, _ = fmt.Fprintf(c.cli.output, "%s\n", output)
+
+	outputFile := c.outputFile
+	if outputFile != nil && !reflect.ValueOf(outputFile).IsNil() {
+		output, err := c.cli.formater.FormatForFile(msg.Type.String(), msg.Data)
+		if err != nil {
+			return err
+		}
+
+		_, err = fmt.Fprintln(outputFile, output)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
-func (ctx *executionContext) Formater() Formater {
-	return ctx.cli.formater
+func (c *executionContext) SendRequest(request string) error {
+	return c.cli.wsConn.Send(c.ctx, request)
 }
 
-func (ctx *executionContext) Connection() ConnectionHandler {
-	return ctx.cli.wsConn
+func (c *executionContext) WaitForResponse(timeout time.Duration) (Message, error) {
+	ctx := c.ctx
+
+	if timeout > 0 {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, timeout)
+		defer cancel()
+	}
+
+	return c.cli.WaitForMessage(ctx)
 }
 
-func (ctx *executionContext) WaitForMessage(c context.Context) (Message, error) {
-	return ctx.cli.WaitForMessage(c)
+func (c *executionContext) EditorMode(initBuffer string) (string, error) {
+	return c.cli.editor.Edit(c.ctx, initBuffer)
 }
 
-func (ctx *executionContext) Editor() Editor {
-	return ctx.cli.editor
+func (c *executionContext) CommandMode(initBuffer string) (string, error) {
+	return c.cli.editor.CommandMode(c.ctx, initBuffer)
 }
 
-func (ctx *executionContext) Factory() CommandFactory {
-	return ctx.cli.cmdFactory
+func (c *executionContext) CreateCommand(raw string) (Executer, error) {
+	return c.cli.cmdFactory.Create(raw)
 }
