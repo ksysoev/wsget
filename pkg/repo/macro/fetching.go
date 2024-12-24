2509a61fbfcbbc7cd4d5030c89bdb9522ad7a789
Kirill Sysoev <k.sysoev@me.com>
2024-12-24 20:41:39 +0800

    Add tests for the Download function in macro package
    Introduce comprehensive unit tests for the Download function, covering various scenarios like successful downloads, error handling, and invalid configurations. These tests ensure robust validation of the function's behavior under different conditions.

--- pkg/repo/macro/fetching.go
+++ pkg/repo/macro/fetching.go
@@ -11,7 +11,6 @@ import (
 // It returns an error if the download fails, the file already exists, the YAML unmarshalling fails, or the macro version is unsupported.
 func Download(filepath, url string) (err error) {
 	resp, err := http.Get(url) //nolint:gosec // This is a CLI tool, and the URL is provided by the user
-
 	if err != nil {
 		return fmt.Errorf("fail to download macro: %w", err)
 	}
